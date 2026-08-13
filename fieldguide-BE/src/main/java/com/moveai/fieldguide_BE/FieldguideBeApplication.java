126e9edee8ae23c643d5e16261593239123aaf41
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-08-13 09:59:43 +0900

    created spring project

---
+++ fieldguide-BE/src/main/java/com/moveai/fieldguide_BE/FieldguideBeApplication.java
@@ -0,0 +1,13 @@
+package com.moveai.fieldguide_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldguideBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldguideBeApplication.class, args);
+	}
+
+}
---
+++ fieldguide-BE/src/test/java/com/moveai/fieldguide_BE/FieldguideBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveai.fieldguide_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldguideBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
