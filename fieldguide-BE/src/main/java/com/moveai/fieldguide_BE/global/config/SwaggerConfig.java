21c92ca482f99ab9e452148c62bc6aebf8b1c0a3
coenfflOo <minipeach0923@gamil.com>
2026-08-13 16:46:04 +0900

    feat: Swagger(springdoc) 설정 추가 및 Spring Boot 4 호환 버전으로 수정

---
+++ fieldguide-BE/src/main/java/com/moveai/fieldguide_BE/global/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.moveai.fieldguide_BE.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fieldguide API")
+                        .description("fieldguide 백엔드 API 문서")
+                        .version("v1.0.0"));
+    }
+}
