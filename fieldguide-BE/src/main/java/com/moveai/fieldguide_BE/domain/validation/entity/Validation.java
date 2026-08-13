4729a1ddfc1e857872c9004b1c9f424821869bc3
coenfflOo <minipeach0923@gamil.com>
2026-08-13 12:57:59 +0900

    feat: created Validation entity

---
+++ fieldguide-BE/src/main/java/com/moveai/fieldguide_BE/domain/validation/entity/Validation.java
@@ -0,0 +1,52 @@
+package com.moveai.fieldguide_BE.domain.validation.entity;
+import com.moveai.fieldguide_BE.domain.member.entity.Member;
+import com.moveai.fieldguide_BE.domain.tacitknowledge.entity.TacitKnowledge;
+import com.moveai.fieldguide_BE.domain.tacitreport.entity.TacitReport;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "validation")
+public class Validation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id")
+    private TacitReport report;
+
+    @ManyToOne
+    @JoinColumn(name = "knowledge_id")
+    private TacitKnowledge knowledge;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private ZonedDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul"));
+    }
+
+}
