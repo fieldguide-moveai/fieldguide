fd498ab5138654a3bdc7d21c43a3907e3417121c
라송연 <skthddus123@naver.com>
2026-08-13 15:33:59 +0900

    fix crossorigin

--- fieldguide-BE/src/main/java/com/moveai/fieldguide_BE/domain/tacitreport/controller/AudioController.java
+++ fieldguide-BE/src/main/java/com/moveai/fieldguide_BE/domain/tacitreport/controller/AudioController.java
@@ -8,16 +8,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/audio")
 public class AudioController {
 
     private final AudioTranscriptionService audioTranscriptionService;
 
     @PostMapping("/transcribe")
-    public String transcribe(
-            @RequestPart("file") MultipartFile file
-    ) throws Exception {
-
+    public String transcribe(@RequestPart("file") MultipartFile file) throws Exception {
         return audioTranscriptionService.transcribe(file);
     }
 }
